5dc39d1933b372dc79b0966c7c2f3861ac97e540
Greg Morgan <gregm9999@gmail.com>
2019-01-18 22:56:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,20 +1,4 @@
 //Your code here
 function justInvoke(fn) {
-  return fn();
-}
-
-function setThisWithCall(fn, thisValue, argument) {
-  return fn.call(thisValue,argument);
-}
-
-function setThisWithApply(fn, thisValue, argument) {
-  return fn.apply(thisValue, argument);
-}
-
-function returnNewFunctionOf(functionToBeCopied, thisValue) {
-<<<<<<< HEAD
-  return functionToBeCopied.bind(thisValue);
-=======
-  return functionToBeCopied.apply(thisValue);
->>>>>>> 948aedd28c7c2adc72a423018c518533c7b3365e
+  return fn;
 }
