c576a0c24a931864bab2900f532f7a3fdf8f1cb3
Greg Morgan <gregm9999@gmail.com>
2019-01-18 23:08:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ function setThisWithApply(fn, thisValue, arg) {
 }
 
 function returnNewFunctionOf(functionToBeCopied, thisValue) {
-  return .functionToBeCopied.bind(thisValue);
+  return functionToBeCopied.bind(thisValue);
 }
