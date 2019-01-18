050233de4732084dec3dd5ef6223ef30122521bd
Greg Morgan <gregm9999@gmail.com>
2019-01-18 23:05:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,7 @@ function setThisWithCall(fn, thisValue, arg) {
 function setThisWithApply(fn, thisValue, arg) {
   return fn.apply(thisValue, arg);
 }
+
+function returnNewFunctionOf(firstArgument, secondArgument) {
+
+}
